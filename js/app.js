9b436d78435cba64d5d5398024560ce8fa62354a
HajjamN <hajjamn@gmail.com>
2024-04-08 21:34:27 +0200

    ADD: emails generation

--- js/app.js
+++ js/app.js
@@ -5,7 +5,25 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'API'
+      message: 'API',
+      emails: []
+    }
+  },
+  mounted() {
+    console.log('prima di generare', this.emails)
+    this.getRandomEmails(10)
+    console.log('Dopo il ciclo', this.emails)
+  },
+  methods: {
+    getRandomEmails(n) {
+      for (let i = 0; i < n; i++) {
+        axios
+          .get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((res) => {
+            console.log(res.data.response)
+            this.emails.push(res.data.response)
+          })
+      }
     }
   }
 }).mount('#app')
